2484a971cd297e2ccff42440c6017ad1a6638ebb
Enes Mete Kafali <emkafali@gmail.com>
2024-05-30 12:32:42 +0300

    Add project files.

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController(IAuthenticationService authenticationService) : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService = authenticationService;
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            AuthenticationResult? authResult = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.Password); 
+
+            // Mapping
+            var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName, authResult.Email, authResult.Token);
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            AuthenticationResult? authResult = _authenticationService.Login(request.Email, request.Password);
+
+            // Mapping
+            var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName, authResult.Email, authResult.Token);
+
+            return Ok(response);
+        }
+    }
+}
